e2f032aceff6beffcbd27591312a45569de2245b
tekayafiras <63792357+tekayafiras@users.noreply.github.com>
2021-02-24 21:05:23 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -147,6 +147,31 @@ slides.forEach(setSlidePosition);
     // });
     
 
+items = document.querySelectorAll('.hide')
+
+const isInViewport = el => {
+  const rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+};
+
+const run = () =>
+  items.forEach(item => {
+    if (isInViewport(item)) {
+      item.classList.add('show');  // ken element yodhher fil page yetzedlou l class "show"
+    }
+  });
+
+// Events
+window.addEventListener('load', run);
+window.addEventListener('resize', run);
+window.addEventListener('scroll', run);  
+
     
 
 
